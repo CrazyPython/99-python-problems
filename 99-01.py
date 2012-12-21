3f7ae69215f1d49426558e99919d27973e68a913
jhrr <james@el-mundo.net>
2012-12-21 11:31:38 +0000

    Exercises 1 and 2

---
+++ 99-01.py
@@ -0,0 +1,13 @@
+# Find the last box of a list.
+
+
+list = [0, 1, 2, 3, 4, 5]
+
+list[-1] # Negative indexing
+
+list[len(ls)-1] # Explicit
+
+# As a function
+
+def last(list):
+    return list[-1]
