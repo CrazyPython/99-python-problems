c25c06707665205e80d1a3bdac02e026369c4e2e
CrazyPython <Jamtlu@gmail.com>
2012-12-23 05:53:25 -0800

    Create 99-06.py

---
+++ 99-06.py
@@ -0,0 +1,9 @@
+def negative(n):
+    away = n-0
+    return 0-away
+def plaindrome(l):
+    num = 0
+    while len(l) != num:
+        if l[num] != l[negative(num+1):
+            return False
+    return True
